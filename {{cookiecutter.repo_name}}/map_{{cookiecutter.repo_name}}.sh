bf6cec7800c2a6030c6d4b6b3a45065a3619ca41
Rob Beagrie <rob@beagrie.com>
2013-11-19 16:29:24 +0100

    Print the current tophat version to stdout first.

--- {{cookiecutter.repo_name}}/map_{{cookiecutter.repo_name}}.sh
+++ {{cookiecutter.repo_name}}/map_{{cookiecutter.repo_name}}.sh
@@ -2,6 +2,9 @@
 #$ -l h_vmem=12G
 #$ -cwd
 
+echo "TopHat version:"
+{{ cookiecutter.tophat_binary }} --version
+
 READS=""
 
 {{ cookiecutter.tophat_binary }} -p {{ cookiecutter.num_processes }} -r {{ cookiecutter.mate_inner_dist }} --mate-std-dev {{ cookiecutter.mate_std_dev }} --library-type {{ cookiecutter.library_type }} -G {{ cookiecutter.gtf_file }} -o mapping_result {{ cookiecutter.bowtie_index }} $READS
